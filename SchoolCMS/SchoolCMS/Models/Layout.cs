407607a78d7ba7f81565aab806b231820ddeaac6
Kamil Ślusarczyk <kamil.slusarczyk@hotmail.com>
2015-04-23 14:54:14 +0200

    Models

---
+++ SchoolCMS/SchoolCMS/Models/Layout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolCMS.Models
+{
+    public class Layout
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+    }
+}
---
+++ SchoolCMS/SchoolCMS/Models/MenuButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolCMS.Models
+{
+    public class MenuButton
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        //public MenuButton Parent {get; set;}
+    }
+}
