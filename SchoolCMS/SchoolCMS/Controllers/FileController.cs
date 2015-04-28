62e177972cf076e8e90975f16c596e2be769269b
Artur Stelmach <arturstelmach92@gmail.com>
2015-04-28 22:29:23 +0200

    Files functionalitiy end

--- SchoolCMS/SchoolCMS/Controllers/FileController.cs
+++ SchoolCMS/SchoolCMS/Controllers/FileController.cs
@@ -12,7 +12,7 @@ namespace SchoolCMS.Controllers
     {
         //
         // GET: /File/
-
+        private const string filesDirectory = "~/App_Data/Uploads/";
         public ActionResult Index()
         {
             return View();
@@ -29,10 +29,63 @@ namespace SchoolCMS.Controllers
         }
 
         [HttpPost]
-        public ActionResult Add(File file)
+        public ActionResult Add(File model,HttpPostedFileBase file)
         {
+            if (file==null||file.ContentLength<=0)
+            {
+                ModelState.AddModelError(string.Empty,"Pusty plik");
+                return View(model);
 
-            return View();
+            }
+            if (!ModelState.IsValid) return View(model);
+
+            var extension = Path.GetExtension(file.FileName);
+            var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+
+            if (context.Files.Select(x=>x.FileName).Contains(fileName))
+            {
+                ModelState.AddModelError(string.Empty, "Plik o takiej nazwie już istnieje");
+                return View(model);
+            }
+            if (context.FileExtensions.Select(x=>x.Extension).Contains(extension))
+            {
+
+                model.Extension = extension;
+                model.FileType = context.FileExtensions.Where(x => x.Extension == extension).Select(x => x.FileType).FirstOrDefault();
+                model.Size = file.ContentLength;
+                model.UploadDateTime = DateTime.Now;
+                model.FileName = fileName;
+                file.SaveAs(GetFilePath(model));
+                context.Files.Add(model);
+                context.SaveChanges();
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Nie obsługiwane rozszerzenie");
+                return View(model);
+            }
+
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var file = context.Files.FirstOrDefault(x => x.Id == id);
+            if (file == null) return HttpNotFound();
+
+            if (System.IO.File.Exists(GetFilePath(file)))
+            {
+                System.IO.File.Delete(GetFilePath(file));
+            }
+            context.Files.Remove(file);
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        private string GetFilePath(File file)
+        {
+            return Path.Combine(Server.MapPath(filesDirectory), file.FileName + file.Extension);
+            
         }
 
     }
