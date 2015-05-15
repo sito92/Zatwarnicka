a3358cd54a4f7fbd160b3f90cd99c876f653f28b
Kamil Ślusarczyk <kamil.slusarczyk@hotmail.com>
2015-05-15 22:11:11 +0200

    Student CRUD 65%

---
+++ SchoolCMS/SchoolCMS/Controllers/Ediary/StudentController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using SchoolCMS.Models.EDiary;
+using SchoolCMS.Models;
+using WebMatrix.WebData;
+
+namespace SchoolCMS.Controllers.Ediary
+{
+    public class StudentController : BaseController
+    {
+        //
+        // GET: /Student/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Student student = context.Users.OfType<Student>().FirstOrDefault(x => x.Id == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.ClassId = new SelectList(context.Classes, "Id", "Name");
+            return View();
+        }
+
+        //
+        // POST: /Student/Create
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(StudentRegisterModel student)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    WebSecurity.CreateUserAndAccount(student.UserName, student.Password, new { Discriminator = "Student", 
+                                                                                           Name = student.Name, 
+                                                                                           Surname = student.Surname, 
+                                                                                           Email = student.Email, 
+                                                                                           });
+                    Roles.AddUserToRole(student.UserName, "Student");
+                    return RedirectToAction("List", "Student");
+                }
+                catch (MembershipCreateUserException e)
+                {
+                    ModelState.AddModelError("", ErrorCodeToString(e.StatusCode));
+                }
+            }
+
+            ViewBag.ClassId = new SelectList(context.Classes, "Id", "Name");
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Student student = context.Users.OfType<Student>().FirstOrDefault(x => x.Id == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClassId = new SelectList(context.Classes, "Id", "Name", student.ClassId);
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(student).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            ViewBag.ClassId = new SelectList(context.Classes, "Id", "Name", student.ClassId);
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Student student = context.Users.OfType<Student>().FirstOrDefault(x => x.Id == id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = context.Users.OfType<Student>().FirstOrDefault(x => x.Id == id);
+            context.Users.Remove(student);
+            context.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            var students = context.Users.OfType<Student>();
+
+            return View(students);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
+            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
+            // a full list of status codes.
+            switch (createStatus)
+            {
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "User name already exists. Please enter a different user name.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return
+                        "A user name for that e-mail address already exists. Please enter a different e-mail address.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "The password provided is invalid. Please enter a valid password value.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "The e-mail address provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "The password retrieval question provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "The user name provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return
+                        "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return
+                        "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                default:
+                    return
+                        "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+            }
+        }
+    }
+}
---
+++ SchoolCMS/SchoolCMS/Models/EDiary/StudentModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolCMS.Models.EDiary
+{
+    public class ChangeStudentPassword : ChangePasswordModel
+    {
+    }
+
+    public class ChangeStudentData : ChangePersonalDataModel
+    {    
+    }
+
+    public class StudentRegisterModel : RegisterModel
+    {
+    }
+
+}
