6947b1a980c58e2d600e01289aa4144bbc6f1e92
RadSzymanski <radszy@wp.pl>
2015-05-17 15:04:55 +0200

    Dodatek do poprzedniego commita

---
+++ SchoolCMS/SchoolCMS/Models/EDiary/TeacherModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolCMS.Models.EDiary
+{
+    public class TeacherModels
+    {
+        public class ChangeTeacherPassword : ChangePasswordModel
+        {
+        }
+
+        public class ChangeTeacherData : ChangePersonalDataModel
+        {
+        }
+
+        public class TeacherRegisterModel : RegisterModel
+        {
+        }
+    }
+}
