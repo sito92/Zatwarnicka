4d9b64ea4a4def2605d993cc937a3d9eed20f8e3
Artur Stelmach <arturstelmach92@gmail.com>
2015-05-17 21:03:43 +0200

    News filtering

--- SchoolCMS/SchoolCMS/Controllers/FilterController.cs
+++ SchoolCMS/SchoolCMS/Controllers/FilterController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using SchoolCMS.Helpers;
 using SchoolCMS.Models;
+using SchoolCMS.ViewModels;
 
 namespace SchoolCMS.Controllers
 {
@@ -11,13 +14,25 @@ namespace SchoolCMS.Controllers
     {
         //
         // GET: /Filter/
-
-        public ActionResult Filter(string Tag)
+        private char hashSplit = '#';
+        public ActionResult Filter(string Tag, int pageNumber = 1)
         {
-            var tag = context.Tags.FirstOrDefault(x => x.Name == Tag);
-            var matchingNewses = context.InformationSources.OfType<News>().AsEnumerable().Where(x => x.Tags.Contains(tag));
+            var filtredNews = new List<News>();
 
-            return View(matchingNewses);
+            var tags = Tag.ToLower().Split(hashSplit).Where(x=>!string.IsNullOrEmpty(x));
+
+            var contextTags = context.Tags.AsEnumerable().Where(x => tags.Contains(x.Name.ToLower()));
+
+            foreach (var contextTag in contextTags)
+            {
+                var matchingTag =
+                    context.InformationSources.OfType<News>().AsEnumerable().Where(x => x.Tags.Contains(contextTag)).Distinct();
+
+                filtredNews.AddRange(matchingTag);
+            }
+
+
+            return View(filtredNews);
         }
 
     }
