7b81f999072f5e6b6c131a598a44068f29fb5539
Kamil Ślusarczyk <kamil.slusarczyk@hotmail.com>
2015-05-17 23:31:39 +0200

    Data annotations register models

--- SchoolCMS/SchoolCMS/Models/CopyWriterModels.cs
+++ SchoolCMS/SchoolCMS/Models/CopyWriterModels.cs
@@ -12,7 +12,7 @@ namespace SchoolCMS.Models
 {
         public class ChangePasswordModel
         {
-            [Required]
+            [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
             [Display(Name = "Nazwa użytkownika do zmiany danych")]
             public string UserName { get; set; }
 
@@ -29,7 +29,7 @@ namespace SchoolCMS.Models
 
         public class ChangePersonalDataModel
         {
-            [Required]
+            [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
             [Display(Name = "Nazwa użytkownika do zmiany danych")]
             public string UserName { get; set; }
 
@@ -45,12 +45,12 @@ namespace SchoolCMS.Models
 
         public class RegisterModel
         {
-            [Required]
+            [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
             [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "Nazwa użytkownika może składać się wyłącznie z liter i cyfr")]
             [Display(Name = "Nazwa użytkownika")]
             public string UserName { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Hasło jest wymagane")]
             [StringLength(100, ErrorMessage = "Hasło może mieć maksymalną długość {2} ", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Hasło")]
@@ -61,18 +61,18 @@ namespace SchoolCMS.Models
             [Compare("Password", ErrorMessage = "Hasła nie pasują")]
             public string ConfirmPassword { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Adres e-mail jest wymagany")]
             //[EmailAddress(ErrorMessage = "Nieprawidłowy adres email")]
             [EmailAddress(ErrorMessage = "Nieprawidłowy format adresu email")]
             [Display(Name = "E-mail")]
             public string Email { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Imie jest wymagane")]
             [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Imie może składać się wyłącznie z liter")]
             [Display(Name = "Imię")]
             public string Name { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Nazwisko jest wymagane")]
             [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Nazwisko może składać się wyłącznie z liter")]
             [Display(Name = "Nazwisko")]
             public string Surname { get; set; }
